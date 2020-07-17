cb740b85239a6ccaa7b4f20391f1dc7c12971601
Rodrigo Passos <rodrigo.passos25@gmail.com>
2020-07-16 22:49:27 -0300

    Iniciado app mobile

---
+++ mobile/src/pages/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Button } from 'react-native';
+
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button onPress={signOut} title="SignOut" />
+    </View>
+  );
+};
+
+export default Dashboard;
