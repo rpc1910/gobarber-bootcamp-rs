ad59860c446abee60118ba6e5eca6162cb3ec0c7
Rodrigo Passos <rodrigo.passos25@gmail.com>
2021-02-16 18:25:10 -0300

    Rotas e controllers

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -9,11 +9,6 @@ appointmentsRouter.use(ensureAuthenticated);
 
 const appointmentsController = new AppointmentsController();
 
-// appointmentsRouter.get('/', async (request, response: Response) => {
-//   const appointments = await appointmentsRepository.find();
-//   return response.json(appointments);
-// });
-
 appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
